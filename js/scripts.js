6b9890774d5f631550538526d6fed4ebe6ff7ad4
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-08 15:24:37 -0700

    have phrase in a single array

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,40 @@
 // user interface:
-var sqrts = [144, 121, 100, 81, 64, 49, 36, 25, 16, 9, 4];
 var originalPhrase = "Hello there! Don't leave.";
-var erasePunc = originalPhrase.replace(/[\W]/g,'').toLowerCase();
+var formattedString = originalPhrase.replace(/[\W]/g,'').toLowerCase();
 
 var makeArray = function(phrase) {
   phraseArray = phrase.split('');
-  console.log(phraseArray);
+//  console.log(phraseArray);
   return phraseArray;
 }
-
 
 var closestSquareRoot = function(phrase) {
   var phraseLength = (phrase.length);
   var squareRoot = Math.sqrt(phraseLength);
-  var roundedNum = Math.ceil(squareRoot);
-  console.log(roundedNum);
+  var roundedNum = Math.floor(squareRoot);
+//  console.log(roundedNum);
+  return roundedNum;
 }
 
-
 var encryptFunction = function(phrase) {
-
+//  console.log(closestSquareRoot(phrase));
+//  return closestSquareRoot(phrase);
+  var newArray = [];
+  for ( ; ; ;) {
+    //save point, logic not yet written
+  }
+  console.log(newArray);
 }
 
 
 $(document).ready(function() {
   $("#submitButton").click(function(event) {
     event.preventDefault();
-    var encryptThis = $("#phrase-input").val();
-    makeArray(erasePunc);
-    var outputResult = closestSquareRoot(erasePunc);
-
+//    var encryptThis = $("#phrase-input").val();
+    makeArray(formattedString);
+    closestSquareRoot(formattedString);
+    var encryptedResult = encryptFunction(phraseArray)
 //    var outputResult = encryptionFunction(makeArray);
-    $(".output").text(outputResult);
+    $(".output").text(encryptFunction);
   });
 });
